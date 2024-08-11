b4da473fdd3aa0356ffd8ee4a5888d13055d310e
hadisql <hadi.sqalli@laplateforme.io>
2024-08-11 14:29:20 +0200

    added search field for Changeset model in admin

--- changesets/admin.py
+++ changesets/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Changeset
 
-admin.site.register(Changeset)
+class ChangesetAdmin(admin.ModelAdmin):
+    list_display = ('changeset_id', 'created_at', 'closed_at', 'open', 'changes_count', 'user', 'user_id', 'min_lat', 'max_lat', 'min_lon', 'max_lon', 'comments_count', 'tags')
+    search_fields  = ['changeset_id']
+
+admin.site.register(Changeset, ChangesetAdmin)
