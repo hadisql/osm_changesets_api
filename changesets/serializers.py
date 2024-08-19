f6b40d6c13603db4c9fc81f61aa518c8f3ab43d7
hadisql <hadi.sqalli@laplateforme.io>
2024-08-19 13:58:13 +0200

    changeset serializer returns all fields except id

--- changesets/serializers.py
+++ changesets/serializers.py
@@ -6,8 +6,10 @@ from .models import Changeset
 class ChangesetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Changeset
-        fields = [
-            'changeset_id', 'created_at', 'closed_at', 'open', 'changes_count', 
-            'user', 'user_id', 'min_lat', 'max_lat', 'min_lon', 'max_lon', 
-            'comments_count', 'additional_tags'
-        ]
+        fields = '__all__'
+        hide_fields = ['id'] # fields to hide from the serializer (corresponds to the Changeset model's id field in the database -> useless)
+
+    # overriding the get_field_names method to remove the fields defined in hide_fields
+    def get_field_names(self, declared_fields, info):
+        diminished_fields = super(ChangesetSerializer, self).get_field_names(declared_fields, info)
+        return [field for field in diminished_fields if field not in self.Meta.hide_fields]
