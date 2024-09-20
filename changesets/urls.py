4f149d0f86f0aca5b9fe7c3c1eee4cf09d265466
hadisql <hadi.sqalli@laplateforme.io>
2024-09-20 16:38:05 +0200

    adding changes_count livestream plot

--- changesets/urls.py
+++ changesets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ChangesetListView, redirect_to_landing_page
+from .views import ChangesetListView, redirect_to_landing_page, update_changeset_view
 
 
 urlpatterns = [
     path('sequence/<int:seq_start>/<int:seq_end>/', ChangesetListView.as_view(), name='changeset-list'),
+    path('update/', update_changeset_view, name='update_changeset_view'),
     path('sequence/', redirect_to_landing_page),
     path('', redirect_to_landing_page),
 ]
