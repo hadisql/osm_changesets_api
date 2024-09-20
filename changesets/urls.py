9a9abd1210473466e5044b076f653cfcbe1a18ca
hadisql <hadi.sqalli@laplateforme.io>
2024-09-20 17:18:34 +0200

    slight fix on urls

--- changesets/urls.py
+++ changesets/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import ChangesetListView, redirect_to_landing_page, update_changeset_view
 
-app_name = 'changesets'
 
 urlpatterns = [
     path('sequence/<int:seq_start>/<int:seq_end>/', ChangesetListView.as_view(), name='changeset-list'),
